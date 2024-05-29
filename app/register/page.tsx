89ee483358217c316fdda0489b2629d795bc0f57
Nathan Sodja <sodjanathan@gmail.com>
2024-05-29 18:43:04 +0000

    working register validation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,23 +4,46 @@ import Image from 'next/image';
 import { AtSymbolIcon, KeyIcon, PencilIcon } from '@heroicons/react/24/outline';
 import { Button } from '../ui/button';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
 
-interface IFormInput {
-  name: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
+// Define the validation schema using Zod
+const schema = zod
+  .object({
+    name: zod.string().min(6, 'Name must be at least 6 characters long'),
+    email: zod.string().email('Invalid email address'),
+    password: zod
+      .string()
+      .min(6, 'Password must be at least 6 characters long')
+      .nullable(),
+    confirmPassword: zod
+      .string()
+      .min(6, 'Confirm password must be at least 6 characters long'),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
+
+type FormInput = zod.infer<typeof schema>;
 
 export default function Register() {
-  const { register, handleSubmit } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
 
   return (
     <main className="flex min-h-screen flex-col p-6">
       {/* form starts here */}
 
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {' '}
         <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
           {/* <AcmeLogo /> */}
           <p className="text-white"> churchday⛪.</p>
@@ -39,51 +62,63 @@ export default function Register() {
             </div>
             <div className="relative">
               <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                className={`peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 ${errors.name ? 'border-red-500' : ''}`}
                 id="name"
                 type="text"
-                name="name"
                 placeholder="Enter name..."
                 required
                 minLength={6}
+                {...register('name')}
               />
               <PencilIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              {errors.name && (
+                <p className="text-xs text-red-500">{errors.name.message}</p>
+              )}
             </div>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="email"
                 type="text"
-                name="email"
                 placeholder="Enter email address"
                 required
                 minLength={6}
+                {...register('email')}
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              {errors.email && (
+                <p className="text-xs text-red-500">{errors.email.message}</p>
+              )}
             </div>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
-                name="password"
                 placeholder="Enter password"
-                required
-                minLength={6}
+                {...register('password')}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              {errors.password && (
+                <p className="text-xs text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
                 id="password"
                 type="password"
-                name="confirmPassword"
                 placeholder="Confirm password"
-                required
-                minLength={6}
+                {...register('confirmPassword')}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              {errors.confirmPassword && (
+                <p className="text-xs text-red-500">
+                  {errors.confirmPassword.message}
+                </p>
+              )}
             </div>
             <Button type={'submit'}>Create account</Button>
           </div>
